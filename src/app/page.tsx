5d17d5a8d233c62398b74d2386d2174946a5601e
heyiming <ymjrcc@qq.com>
2024-08-29 17:06:21 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import dynamic from 'next/dynamic';
 
-const Map = dynamic(import('../components/Map'), { ssr: false });
+const Map = dynamic(() => import('../components/Map'), { ssr: false });
 
 export const metadata: Metadata = {
   title: 'Home | EchoEcho',
